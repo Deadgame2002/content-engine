286fc0a8fce76ff000861031dadbd6ac344188e3
Deadgame2002 <msiarnolds14@gmail.com>
2026-06-25 02:00:11 +0300

    new site pages

--- sites/site3/_data/site.js
+++ sites/site3/_data/site.js
@@ -1,6 +1,7 @@
 module.exports = {
   site_name: process.env.SITE_NAME || "My Craft Blog",
   site_description: process.env.SITE_DESCRIPTION || "Fonts, SVGs and craft ideas",
+  contact_email: process.env.SITE_CONTACT_EMAIL || "",
   ref_link: "https://www.creativefabrica.com/ref/8793785/",
   // Укажи реальный домен сайта в переменной окружения SITE_BASE_URL в настройках Cloudflare Pages,
   // например https://fonts-for-cricut.pages.dev или https://твой-домен.com (без слэша на конце)
